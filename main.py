56c7a56cdd4f22cefda19983854c0198365d839f
Nikolay Sirotkin <sirotkin.nikola@mail.ru>
2023-01-22 21:45:46 +0300

    Task complete.

---
+++ main.py
@@ -0,0 +1,18 @@
+import nltk
+import pymorphy2
+nltk.download('punkt')
+
+prob_thresh = 0.4
+
+morph = pymorphy2.MorphAnalyzer()
+name_list = []
+with open('text.txt', 'r', encoding='Utf8') as text:
+    new_text = text.read()
+    for word in nltk.word_tokenize(new_text):
+        for p in morph.parse(word):
+            if 'Name' in p.tag and p.score >= prob_thresh:
+                if p.normal_form.title() not in name_list:
+                    name_list.append(p.normal_form.title())
+print('Найденные имена в тексте: ')
+for result_name in name_list:
+    print(result_name)
